86ed128fbb0990c7363232f806c56223641e5d84
alexFiorenza <alexfiorenza2012@gmail.com>
2020-04-26 00:29:01 -0300

    Finished testing some things in socket I think that is time to start with frontend :)

--- controllers/sockets/sockets.js
+++ controllers/sockets/sockets.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { io } = require('../../index');
+const Publication = require('../../models/publication');
+const { verifyToken } = require('../../Middlewares/auth');
 io.on('connection', (client) => {
   console.log('succesfully connected');
+  client.on('btnClicked', (data) => {
+    console.log(data);
+  });
 });
 
 module.exports = router;
